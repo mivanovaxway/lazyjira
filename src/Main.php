0f756d2bdb14e4983f95f1db381704585f53fbfa
Mihail Ivanov <mivanov@axway.com>
2018-04-13 13:44:36 +0300

    Initial commit

---
+++ src/Main.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Mivanov\LazyJira;
+
+class Main
+{
+
+    public function run()
+    {
+        echo "ok";
+    }
+}
---
+++ src/run.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Mivanov\LazyJira;
+
+require_once(__DIR__ . "/../vendor/autoload.php");
+
+$main = new Main();
+$main->run();
